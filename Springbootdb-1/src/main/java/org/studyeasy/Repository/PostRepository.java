6a6ce05e85cea57b4b54be8a658bd063db114f99
Prathyusha-Gaddam <105450599+Prathyusha-Gaddam@users.noreply.github.com>
2023-05-09 23:28:44 +0530

    Delete Springbootdb-1 directory

--- Springbootdb-1/src/main/java/org/studyeasy/Repository/PostRepository.java
+++
@@ -1,9 +0,0 @@
-package org.studyeasy.Repository;
-
-import org.springframework.data.repository.CrudRepository;
-import org.studyeasy.entity.Post;
-
-public interface PostRepository extends CrudRepository<Post,Integer>{
-	
-
-}
